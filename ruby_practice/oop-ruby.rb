df79896c12ea0b0e93c726a0053d0436b24f25e3
NARISSA <nkh239@nyu.edu>
2020-08-15 14:28:27 -0400

    Inheritance

--- ruby_practice/oop-ruby.rb
+++ ruby_practice/oop-ruby.rb
@@ -4,7 +4,7 @@
 #creating the definition of a dog
 class Dog
   #### CLASS VARIABLES ####
-  @@totalDogs = 0
+  @@totalDogs = 0 #These are specific to the class
 
   #### CONSTRUCTOR - always the initialize function ###
   def initialize(name)
@@ -25,27 +25,38 @@ class Dog
   end
 
   #setters - write
-  def setLegs(value)
+  def set_legs(value)
     @legs = value
   end
   def name
     @name
   end
 
-  #class function
+
+  #Class function -- this will return total dogs
   def Dog.total
     @@totalDogs
   end
 
+  def Dog.stuff(my_arg)
+    puts my_arg
+  end
+
 end
 
-Dog1 = Dog.new("Spot") #Creates a new object (aka an instance) called 'Spot'. It's data type will be Dog.
-Dog2 = Dog.new("Fluffy")
 
-Dog2.setLegs(3) #Changes the amount of legs
+# Inheritance
+class Small_Dog < Dog
+  stuff :a_thing
+  # We are just passing symbols in new methods that were pre-defined in the parent class
+end
+
+Dog1 = Small_Dog.new("Spot") #Creates a new object (aka an instance) called 'Spot'. It's data type will be Dog.
+Dog2 = Small_Dog.new("Fluffy")
+
+Dog2.set_legs(3) #Changes the amount of legs
 
 # We can make multiple dogs
 puts Dog1.name
 puts Dog2.name
 puts Dog.total
-
