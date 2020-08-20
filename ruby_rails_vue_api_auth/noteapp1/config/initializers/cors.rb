c211078e011476a6354e139df2ef28525e1f6d83
NARISSA <nkh239@nyu.edu>
2020-08-19 23:18:10 -0400

    Configs and CORS

--- ruby_rails_vue_api_auth/noteapp1/config/initializers/cors.rb
+++ ruby_rails_vue_api_auth/noteapp1/config/initializers/cors.rb
@@ -1,3 +1,6 @@
+# CORS is cross origin resource sharing - security measure that is built into browsers
+# We need to specify which URLS can make requests to your serverrs
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -5,12 +8,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' #Anyone can make a request to the server and they can request anything.
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
